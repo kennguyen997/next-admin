926c9d5ff747de1a590735ee9bbd1fa6aba83a4e
BM Ken <bmken@BMs-iMac-2.local>
2023-02-08 10:34:16 +0700

    add scss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
-
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 module.exports = nextConfig
